7d1c80f28943eea623fe651c63acefb6b110dbe9
BronHarr <56171709+BronHarr@users.noreply.github.com>
2020-04-09 23:55:46 -0400

    Added to RecipeViewFragment by making ingredient list clickable, showing a dialog that will be used for displaying ingredient prep diagrams in a horizontal scroll view.

--- app/src/main/java/edu/fsu/cs/littlechef/RecipeViewFragment.java
+++ app/src/main/java/edu/fsu/cs/littlechef/RecipeViewFragment.java
@@ -1,6 +1,7 @@
 package edu.fsu.cs.littlechef;
 
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,7 +10,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 
@@ -33,9 +36,22 @@ public class RecipeViewFragment extends Fragment {
         ArrayAdapter<String> adapter;
          testlist = getResources().getStringArray(R.array.ingredients);
          ListView Ingredient = (ListView) view.findViewById(R.id.foodlist);
+        Ingredient.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog.Builder prepbuilder = new AlertDialog.Builder(getActivity());
+                View prepView = getLayoutInflater().inflate(R.layout.ingredient_prep_dialog, null);
+                ImageView ingredImg = prepView.findViewById(R.id.ingredientImg);
+                ingredImg.setImageResource(R.drawable.onion_background);
+
+                prepbuilder.setView(prepView);
+                AlertDialog dialog = prepbuilder.create();
+                dialog.show();
+            }
+        });
 
 
-         adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, testlist);
+         adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, testlist);
          Ingredient.setAdapter(adapter);
 
 
