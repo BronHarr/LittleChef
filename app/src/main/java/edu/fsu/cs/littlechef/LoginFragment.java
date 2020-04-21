85e01a832bffbc5178252f6dc9c1d62ba222afac
Megan Primavera <mprimavera317@gmail.com>
2020-04-21 17:29:30 -0400

    ReAdded Register Fragment, app is currently crashing, debugger is angry at recipe Database.

--- app/src/main/java/edu/fsu/cs/littlechef/LoginFragment.java
+++ app/src/main/java/edu/fsu/cs/littlechef/LoginFragment.java
@@ -1,8 +1,13 @@
 package edu.fsu.cs.littlechef;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -12,13 +17,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+
 public class LoginFragment extends Fragment {
     private EditText email, pass;
     private Button login_button;
     private Button register_button;
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+
 
     public LoginFragment() {
         // Required empty public constructor
@@ -35,6 +49,7 @@ public class LoginFragment extends Fragment {
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.login, container, false);
         login_button = rootView.findViewById(R.id.login);
+        register_button = rootView.findViewById(R.id.button);
         email = rootView.findViewById(R.id.email);
         pass = rootView.findViewById(R.id.password);
 
@@ -50,11 +65,31 @@ public class LoginFragment extends Fragment {
 
                 String emailId = email.getText().toString();
                 String password = pass.getText().toString();
-
+                LoginUser(emailId, password);
             }
         });
-
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                Fragment mFrag = new RegisterFragment();
+                ft.replace(R.id.Frag_container, mFrag);
+                ft.commit();
+            }
+        });
         return rootView;
+    }
+
+    private void LoginUser(String mail, String pass) {
+        mAuth.signInWithEmailAndPassword(mail, pass).addOnCompleteListener((Activity) getContext(), new OnCompleteListener<AuthResult>(){
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Intent intent = new Intent(getActivity(), RecipeListActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 
     //function to see if EditText is empty or not.
